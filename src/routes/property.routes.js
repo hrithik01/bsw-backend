71b6643b581c3f50add5ed92e2274d461b426c20
Hrithik Joshi <40289232+hrithik01@users.noreply.github.com>
2024-02-18 19:44:31 +0530

    Add write access key middleware to routes

--- src/routes/property.routes.js
+++ src/routes/property.routes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { propertyController } from '../controllers/property.controller.js'
+import { checkWriteAccessKeyMiddleware } from '../utils/middlewares.js'
 
 const propertyRouter = express.Router()
 
-propertyRouter.post('/', propertyController.createProperty)
+propertyRouter.post('/', checkWriteAccessKeyMiddleware, propertyController.createProperty)
 propertyRouter.get('/', propertyController.getProperties)
-propertyRouter.post('/update/:property_id', propertyController.updateProperty)
+propertyRouter.post('/update/:property_id', checkWriteAccessKeyMiddleware, propertyController.updateProperty)
 
 export default propertyRouter
--- src/utils/middlewares.js
+++ src/utils/middlewares.js
@@ -8,3 +8,14 @@ export function checkDeleteAccessKeyMiddleware(req, res, next) {
         res.status(401).json({ message: 'Invalid Key' });
     }
 }
+
+export function checkWriteAccessKeyMiddleware(req, res, next) {
+    const key = req.headers['x-access-key'];
+    if (!key) {
+        res.status(401).json({ message: 'x-access-key required in headers to write' });
+    } else if (key === process.env.WRITE_ACCESS_KEY) {
+        next();
+    } else {
+        res.status(401).json({ message: 'Invalid Key' });
+    }
+}
